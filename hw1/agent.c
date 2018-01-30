5ce8e6042aa69eec278883f4a7380497cccac69a
Akhil Sangem <akhilsangem@gmail.com>
2018-01-29 23:09:13 -0600

    Preparing tcp socket

--- hw1/agent.c
+++ hw1/agent.c
@@ -25,11 +25,35 @@ void *heartBeat(void *arg){
   b.IP[2] = '0';
   b.IP[3] = '0';
   b.CmdPort = 9003;
-  while (1) {
-      usleep(60000000);
-      printf("sending heartBeat to manager\n");
+
+  int network_socket;
+  network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in server_address;
+
+  server_address.sin_family = AF_INET; //sets type of address
+  server_address.sin_port = htons(9002); //sets port
+  server_address.sin_addr.s_addr = INADDR_ANY; //connects to localhost
+
+  int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+  if (connection_status == -1) {
+    printf("There was an error making a connection to the remote socket \n\n", );
   }
 
+  char server_response[256]; //just made a variable to store the data we got back from the receive
+
+  recv(network_socket, &server_response, sizeof(server_response), 0);
+
+  printf("The server sent the data: %s\n", server_response);
+
+  while (1) {
+    usleep(60000000);
+    printf("sending heartBeat to manager...\n");
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+  }
+
+  close(network_socket);
   return NULL;
 }
 
